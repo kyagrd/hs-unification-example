bb0aae74d3f9bb31e49212c48bc40acc68402b80
Ki Yung Ahn <kyagrd@gmail.com>
2017-06-28 10:40:20 +0800

    small change

--- src/Main.hs
+++ src/Main.hs
@@ -4,6 +4,21 @@ import           Unbound.LocallyNameless
 import           Unif1
 -- import           Unif2
 import qualified Data.Map.Strict as M
+import           Data.List (nub)
+
+
+-- following are all aeq
+aaa = permbind (nub $ fv rule1 :: [Nm]) rule1
+bbb = permbind [x,y] rule1
+ccc = permbind [y,x] rule1
+ddd = rulebind rule1
+
+
+rule1 = (dec[enc[V x,V y], V y],  V x)
+rulebind rule = permbind (nub $ fv rule :: [Nm]) rule
+
+dec = D"dec"
+enc = D"enc"
 
 x,y,z :: Nm
 x = s2n "x"
