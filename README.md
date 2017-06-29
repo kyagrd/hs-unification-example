1a919bc30f4c818653930a942a9ca15675ee4e60
Ki Yung Ahn <kyagrd@gmail.com>
2017-06-29 17:47:30 +0800

    unification modulo subterm convergent rewriting

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # hs-unification-example
 Implementation of the rule-based unification algorithm U using unbound.
 
-Here is how to run the unification `f(x,f(y)) =?= f(z,x)`.
+Here is how to run the unification `f(x,f(y)) =?= f(z,x)` using `Unif1` module.
 ```
 kyagrd@kyagrd:~/github/kyagrd/hs-unification-example$ stack ghci
 ...
@@ -39,4 +39,35 @@ Running the above failing example step-by-step:
 ([Eq (V z) (D "f" [D "f" [D "f" [V z]]])],fromList [(x,D "f" [V y]),(y,D "f" [V z])])
 *Main> ustep it
 *** Exception: user error (z occurs in D "f" [D "f" [D "f" [V z]]])
+```
+
+There is an alternative implementation (`Unif2` module) of the algorithm U,
+which expands the substitution mapping instead of expanding the rest of the equations.
+The advantage of this alternative implementation is that "localized" to
+the substitution mapping instead of repetitive cascading application of
+substitutions to the rest of the equations. `Unif2` implementation can
+easily be refactored to abstract the substitution mapping via the state monad.
+
+# Implementing unification modulo subterm convergent rewriting
+
+`Main` module contains an implementation of unification modulo subterm convergent rewriting,
+which is a Haskell implementation of [SubVariant](http://www.lsv.fr/~ciobaca/subvariant/).
+Here is a run of the same example form the SubVariant homepage:
+```
+*Main> ruleSet1
+[bind [x,y](D "dec" [D "enc" [V 0@0,V 0@1],V 0@1],V 0@0)]
+
+*Main> let t1 = D "dec" [V x,V y]; t2 = D "dec" [V z,V y]
+
+*Main> mapM_ print $ nub $ fmap (\(a,b,c)->(a,c)) $ kleeneClosure (narrBy ruleSet1) (D"_"[t1,t2],subtermInit(D"_"[t1,t2]),emptyMap)
+(D "_" [D "dec" [V x,V y],D "dec" [V z,V y]],fromList [])
+(D "_" [V x4,D "dec" [V z,V y3]],fromList [(x,D "enc" [V x4,V y3]),(y,V y3)])
+(D "_" [D "dec" [V x,V y3],V x4],fromList [(y,V y3),(z,D "enc" [V x4,V y3])])
+(D "_" [V x4,V x4],fromList [(x,D "enc" [V x4,V y3]),(y,V y3),(z,D "enc" [V x4,V y3])])
+
+*Main> mapM_ print $ nub $ unifiersModulo ruleSet1 (t1,t2)
+fromList [(x,V z)]
+fromList [(x,D "enc" [D "dec" [V z,V y3],V y3]),(x4,D "dec" [V z,V y3]),(y,V y3)]
+fromList [(x4,D "dec" [V x,V y3]),(y,V y3),(z,D "enc" [D "dec" [V x,V y3],V y3])]
+fromList [(x,D "enc" [V x4,V y3]),(y,V y3),(z,D "enc" [V x4,V y3])]
 ```
--- src/Main.hs
+++ src/Main.hs
@@ -1,7 +1,10 @@
 module Main where
 import           Control.Applicative
+import           Data.Foldable
 import           Data.List
 import qualified Data.Map.Strict         as M
+import           Data.Set                (Set (..))
+import qualified Data.Set                as S
 import           Syntax
 import           Unbound.LocallyNameless hiding (empty)
 -- import           Unif1
@@ -15,44 +18,73 @@ bbb = permbind [x,y] rule1
 ccc = permbind [y,x] rule1
 ddd = rulebind rule1
 
-rules = map rulebind [rule1]
-rule1 = (dec[enc[V x,V y], V y],  V x)
+
 rulebind rule = permbind (nub $ fv rule :: [Nm]) rule
-
-choose [] = empty
-choose xs = do (xs1,x:xs2) <- init $ zip (inits xs) (tails xs)
-               return (x, xs1++xs2)
-
-subterm t@(V _)    = return (id, t)
-subterm t@(D f []) = return (id, t)
-subterm t@(D f ts) = return (id, t) <|>
-  do (ts1,u:ts2) <- init $ zip (inits ts) (tails ts)
-     let stctx x = D f $ ts1++x:ts2
-     (stctx', u') <- subterm u
-     return (stctx . stctx', u')
-
--- non variable positions only
-subtermInit t = do { r@(stctx, D _ _) <- subterm t; return r }
-
-
-mytm' = D"_"[mytm,V x,V y]
-mytm = dec[V x,V y]
-
--- implementation of narrowing from
--- https://pdfs.semanticscholar.org/1803/a29f9588026a731a1baf4cc61a0d328d3e06.pdf
-narr (t,positions,ss) =
-  do ((stctx, t), ps) <- choose positions
-     rule <- rules
-     runFreshMT $
-       do (_,(l,r)) <- unbind rule
-          s <- u ([l`Eq`t],emptyMap)
-          let subs = expand s
-          return (subs . stctx . subs $ r, ps, s:ss)
-
 
 dec = D"dec"
 enc = D"enc"
+rule1 = (dec[enc[V x,V y], V y],  V x)
+ruleSet1 = map rulebind [rule1]
 
+
+mytmDec2 = D"_"[dec[V x,V y],dec[V z,V y]]
+mytmDec1 = dec[V x,V y]
+mytmDec1' = D"_"[mytmDec1,V x,V y]
+
+ruleSet2 = map rulebind
+  [ (D"h"[D"f"[V x],V y],  V y)
+  , (D"h"[D"g"[V x],V y],  V y)
+  ]
+
+mytmH' = D"_"[mytmH,V x,V y]
+mytmH = h[V x,V y] where h=D"h"
+
+data Pos = PR | PD Int Pos deriving (Eq,Ord,Show)
+
+plugPos _        PR         v = v
+plugPos (D f ts) (PD k pos) v = D f $ ts1 ++ plugPos u pos v : ts2
+  where (ts1,u:ts2) = splitAt k ts
+
+subterm t@(V _)    = return (PR, t)
+subterm t@(D f []) = return (PR, t)
+subterm t@(D f ts) = return (PR, t) <|>
+  do k <- [0 .. length ts - 1]
+     (pos, v) <- subterm (ts!!k)
+     return (PD k pos, v)
+
+-- non variable positions only
+subtermInit t = do { r@(pos, D _ _) <- subterm t; return pos }
+
+atPos PR         t        = t
+atPos (PD k pos) (D _ ts) = atPos pos (ts!!k)
+
+-- implementation of narrowing from
+-- https://pdfs.semanticscholar.org/1803/a29f9588026a731a1baf4cc61a0d328d3e06.pdf
+narrBy rules (t,poss,s) =
+  do pos <- poss
+     rule <- rules
+     runFreshMT $
+       do mapM_ fresh (nub $ fv t :: [Nm])
+          (_,(l,r)) <- unbind rule
+          s1 <- u ([l `Eq` atPos pos t],emptyMap)
+          let s2 = foldr extend s (M.toList s1)
+          let subs = expand s1
+          return (plugPos (subs t) pos (subs r), poss\\[pos], s2)
+     where
+       extend (x,t) s = let t' = expand s t
+                         in M.insert x t' (subst x t' <$> s)
+
+kleeneClosure step x = return x <|> transClosure step x
+transClosure step x = xs' <|> asum (transClosure step <$> xs')
+  where xs' = step x
+
+-- example unification (finding unifier modulo subterm convergnet rewriting)
+-- basically implmented http://www.lsv.fr/~ciobaca/subvariant/
+
+unifiersModulo rules (t1,t2) = do
+  (D"_"[t1',t2'], _, s) <- kleeneClosure (narrBy rules)
+                            (D"_"[t1,t2], subtermInit $ D"_"[t1,t2], emptyMap)
+  u ([t1' `Eq ` t2'], s)
 
 x,y,z :: Nm
 x = s2n "x"
